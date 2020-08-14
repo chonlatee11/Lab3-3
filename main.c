ac78d427635e60e24df1ea685af636ec0988ffc7
chonlatee11 <chonlatee1129@gmail.com>
2020-08-14 12:19:49 +0000

    .2

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
+//#include <math.h>
 int main(void) {
-  printf("Hello World\n");
+  float a, b;
+  printf("Enter A : ");
+  scanf("%f", &a);
+  printf("Enter B : ");
+  scanf("%f", &b);
+  printf("%.2f * %.2f = %.2f", a, b, a*b);
   return 0;
 }
